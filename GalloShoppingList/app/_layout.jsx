5dd1e2ea6e14282403b2b95bc6e697bee49ee84b
7diegojr <dfsntsjunior@gmail.com>
2025-05-12 11:40:51 -0300

    criando o footer

--- GalloShoppingList/app/_layout.jsx
+++ GalloShoppingList/app/_layout.jsx
@@ -1,14 +1,14 @@
-import {Stack} from 'expo-router'
+import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 
 export default function RootLayout() {
-  return (
+	return (
 		<>
-		<Stack>
-			<Stack.Screen name='index' options={{headershown: false}}/>
-			<Stack.Screen name='home' options={{headershown: false}}/>
-		</Stack>
-		<StatusBar sttyle='light'/>
+			<Stack>
+				<Stack.Screen name='index' options={{ headerShown: false }} />
+				<Stack.Screen name='home' options={{ headerShown: false }} />
+			</Stack>
+			<StatusBar style='light' />
 		</>
-  )
+	)
 }
